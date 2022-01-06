945861521bed19e4e6891b2a95ec339930df67f4
Zims <zims@inbox.lv>
2022-01-06 22:40:30 +0200

    few queries

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ import sqlite3
 
 app = FastAPI()
 
+# Database connection
 conn = sqlite3.connect('ss_db.sqlite3')
 c = conn.cursor()
 
 # queries
-c.execute("SELECT * FROM ss_appartments")
-whole_db = c.fetchall()
-c.execute("SELECT * FROM ss_appartments WHERE region='yugla'")
-yugla_db = c.fetchall()
+c.execute("SELECT * FROM ss_appartments ORDER BY id DESC")
+appartments = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
 
+c.execute("SELECT * FROM ss_appartments WHERE region = 'yugla' ORDER BY id DESC")
+yugla_appartments = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
+
+
+# Endpoints #
 
 @app.get("/")
 def index():
@@ -21,8 +25,8 @@ def index():
 
 @app.get("/all")
 def centrs():
-    return whole_db
+    return appartments
 
 @app.get("/yugla")
 def yugla():
-    return yugla_db
+    return yugla_appartments
