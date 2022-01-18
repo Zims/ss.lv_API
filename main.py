a8519f7d6a14a4d24a7dc52b5e297feb6a7a519b
Zims <zims@inbox.lv>
2022-01-18 11:04:40 +0200

    api changes

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
-import json
 import sqlite3
 
 
 app = FastAPI()
 
 # Database connection
-conn = sqlite3.connect('ss_db.sqlite3', check_same_thread=False)
+conn = sqlite3.connect('scrapers/ss_all.sqlite3', check_same_thread=False)
 c = conn.cursor()
 
 # queries
-c.execute("SELECT * FROM ss_appartments ORDER BY id DESC")
+c.execute("SELECT * FROM ss_all ORDER BY id DESC")
+# get 20 newest records
+c.execute("SELECT * FROM ss_all ORDER BY id DESC LIMIT 50")
+
 all_appartments = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
 
 
@@ -25,6 +27,6 @@ def get_place(place):
     if place == "all":
         return all_appartments
     else:
-        c.execute("SELECT * FROM ss_appartments WHERE region = ? ORDER BY id DESC", (place,))
+        c.execute("SELECT * FROM ss_all WHERE price = ? ORDER BY id DESC", (place,))
         place_appartments = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
         return place_appartments
