5a40e95470b5e8cf550855fc1fabe66b79a681b7
Zims <zims@inbox.lv>
2022-04-04 10:50:29 +0300

    new routes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sqlite3
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from datetime import datetime
+
 
 
 app = FastAPI()
@@ -12,9 +14,9 @@ conn = sqlite3.connect('scrapers/ss_all.sqlite3', check_same_thread=False)
 c = conn.cursor()
 
 # queries
-c.execute("SELECT * FROM ss_all ORDER BY id DESC")
+c.execute("SELECT * FROM ss_all_new ORDER BY id DESC")
 # get 20 newest records
-c.execute("SELECT * FROM ss_all ORDER BY id DESC LIMIT 50")
+c.execute("SELECT * FROM ss_all_new ORDER BY id DESC LIMIT 50")
 
 all_appartments = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
 
@@ -26,28 +28,49 @@ templates = Jinja2Templates(directory="templates")
 async def docs_redirect():
     return RedirectResponse(url='/docs')
 
-# Opens index with a link to docs
-# @app.get("/", response_class=HTMLResponse)
-# async def index(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/today")
+async def today(request: Request):
+    todays_date = datetime.now().strftime("%Y-%m-%d")
+    c.execute("SELECT * FROM ss_all_new WHERE date_added = ? ORDER BY added_to_db DESC", (todays_date,))
+    # put them in a list
+    todays_ads = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
+    return todays_ads
+
 
 @app.get("/places")
 async def places(request: Request):
     # return all places in database
     # only unique values for rajons
-    c.execute("SELECT DISTINCT rajons FROM ss_all")
+    c.execute("SELECT DISTINCT district FROM ss_all_new")
     # put them in a list
-    rajons = [row[0] for row in c.fetchall()]
-    return rajons
+    district = [row[0] for row in c.fetchall()]
+    return district
+
+
+@app.get("/{district}/{end_date}")
+async def district(request: Request, district: str, end_date: str):
+    district = district.replace('_', ' ')
+    end_date = end_date.replace('_', '-')
+    c.execute("SELECT * FROM ss_all_new WHERE district = ? AND date_added >= ? ORDER BY added_to_db DESC", (district, end_date))
+    # put them in a list
+    district_ads = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
+    return district_ads
+
 
 @app.get("/{place:str}")
 def get_place(place):
-    if place == "all":
+    if place == "top100":
         # select only top 100 newest records
-        c.execute("SELECT * FROM ss_all ORDER BY date_added DESC LIMIT 100")
-        all_100 = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
-        return all_100
+        c.execute("SELECT * FROM ss_all_new ORDER BY date_added DESC LIMIT 100")
+        top100 = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
+        return top100
+    # elif place == "all":
+    #         # select entire_db newest records
+    #     c.execute("SELECT * FROM ss_all_new ORDER BY date_added DESC")
+    #     entire_db_resp = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
+    #     return entire_db_resp
     else:
-        c.execute("SELECT * FROM ss_all WHERE rajons = ? ORDER BY date_added DESC", (place,))
+        c.execute("SELECT * FROM ss_all_new WHERE district = ? ORDER BY added_to_db DESC LIMIT 300", (place,))
         place_appartments = [dict(zip([key[0] for key in c.description], row)) for row in c.fetchall()]
         return place_appartments
